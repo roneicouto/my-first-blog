55f8490b554b3ad9157a2ff3baee391364edd324
roneicouto <registros@roneicouto.com>
2015-12-04 13:47:31 -0300

    Adicionadas as views para listagem dos posts no blog

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 
 from django.db import models
 
+from django.shortcuts import render
+
 # Create your models here.
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
